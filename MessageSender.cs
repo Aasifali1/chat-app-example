ff79991834ac550c74084734881efe0c668147e4
Aasif Ali <aasif.ali@knoldus.com>
2024-01-10 21:52:47 +0530

    added chat app

---
+++ MessageSender.cs
@@ -0,0 +1,17 @@
+﻿using ChatAppExample.Services;
+
+namespace ChatAppExample
+{
+    public class MessageSender
+    {
+        private readonly NotificationService _notificationService;
+        MessageSender(NotificationService notificationService) 
+        { 
+            _notificationService = notificationService;
+        }
+        public void Send(string message)
+        {
+
+        }
+    }
+}
